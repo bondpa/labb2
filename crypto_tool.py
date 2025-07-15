cb501dfc1d5590df4ccc509c7c7ee6cbe5de0b0f
Backa Elias Bondpä <backaeliasbondpa@gmail.com>
2025-07-15 14:29:06 +0200

    init repo and write generate_key

---
+++ crypto_tool.py
@@ -0,0 +1,5 @@
+# Använder argparse för att hantera kommandoradsalternativ 
+# och utför följande funktioner:
+# 1. Krypterar en fil med en befintlig nyckel.
+# 2. dekrypterar en krypterad fil och återställer originalet
+
