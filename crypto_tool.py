863b99ad1b3f95aecaa3b8b9e8d2ab652ae2c1f5
Backa Elias Bondpä <backaeliasbondpa@gmail.com>
2025-07-15 15:24:02 +0200

    Add decrypt function

--- crypto_tool.py
+++ crypto_tool.py
@@ -30,6 +30,23 @@ def encrypt(file_path, key):
 
 def decrypt(file_path, key):
     print(f"Dekrypterar {file_path} med nyckeln {key}")
+    with open(file_path, "rb") as f:
+        file = f.read()
+
+    with open(key, "rb") as k:
+        key = k.read()
+    
+    fernet = Fernet(key)
+    dec_file = fernet.decrypt(file)
+
+    print(dec_file)
+
+    dec_file_path = file_path + ".dec"
+
+    with open(dec_file_path, "wb") as f:
+        f.write(dec_file)
+
+    print(f"Dekrypterad fil sparad som {dec_file_path}")
 
 
 def main():
