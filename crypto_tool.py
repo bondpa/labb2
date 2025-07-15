3874acb1c1dac9446287505024d821690c79acb2
Backa Elias Bondpä <backaeliasbondpa@gmail.com>
2025-07-15 14:49:55 +0200

    Add arg parsing and function stubs

--- crypto_tool.py
+++ crypto_tool.py
@@ -3,3 +3,23 @@
 # 1. Krypterar en fil med en befintlig nyckel.
 # 2. dekrypterar en krypterad fil och återställer originalet
 
+import argparse
+
+def encrypt(file_path, key):
+    print(f"Krypterar {file_path} med nyckeln {key}")
+    
+
+def decrypt(file_path, key):
+    print(f"Dekrypterar {file_path} med nyckeln {key}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Kryptera eller dekryptera fil")
+    parser.add_argument("action", choices=["encrypt", "decrypt"], help="Kryptera eller dekryptera filen")
+    parser.add_argument("file", help="Sökväg till filen")
+    parser.add_argument("key", help="Sökväg till nyckeln")
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
